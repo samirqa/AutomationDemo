083710fd76cd28e29d32183747acba396a750f8e
Sam <samir.9898580540@gmail.com>
2018-04-28 19:15:08 +0530

    Update

--- src/utility/Constant.java
+++ src/utility/Constant.java
@@ -10,18 +10,16 @@ public class Constant {
 
 	// Test Data Sheet Columns
 	public static final int Col_TestCaseName = 0;
-	public static final int Col_UserName = 1;
+	public static final int Col_Email = 1;
 	public static final int Col_Password = 2;
 	public static final int Col_Browser = 3;
-	public static final int Col_ProductType = 4;
-	public static final int Col_ProductNumber = 5;
 	public static final int Col_FirstName = 6;
 	public static final int Col_LastName = 7;
 	public static final int Col_Address = 8;
 	public static final int Col_City = 9;
 	public static final int Col_Country = 10;
 	public static final int Col_Phone = 11;
-	public static final int Col_Email = 12;
 	public static final int Col_Result = 13;
 	public static final String Path_ScreenShot = System.getProperty("user.dir") + "\\src\\screenshots";
+	public static final String FF_Driver =  System.getProperty("user.dir") + "\\src\\drivers\\geckodriver-v0.19.1-win64\\geckodriver.exe";
 }
